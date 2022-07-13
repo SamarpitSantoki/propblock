45f7c3ef39b1c5cb1266074ff71db6f807531796
SamarpitSantoki <samarpit.santoki@gmail.com>
2022-07-13 16:39:35 +0530

    Initial client and backend setup

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+//this is to catch all path for client
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+//make apis with /api prefix
+
+app.listen(9000);

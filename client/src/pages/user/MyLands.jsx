db0e841ecd11377e5242ad859595ed750e92ed0b
Pranav Savani <103499639+pranav1517@users.noreply.github.com>
2022-11-03 02:21:47 +0530

    Mylands table created

--- client/src/pages/user/MyLands.jsx
+++ client/src/pages/user/MyLands.jsx
@@ -1,5 +1,39 @@
 // page for viewing User's lands in table format
+
 function MyLands() {
-  return <div>MyLands</div>;
+  return <div>
+    <div className="col-10 m-auto mt-5 border-top border-end border-start">
+ <table class="table  table-hover" style={{backgroundColor: 'white'}}>
+  <thead style={{backgroundColor:'#cfcfcf'}}>
+    <tr>
+      <th scope="col">Sr. No.</th>
+      <th scope="col">Address</th>
+      <th scope="col">Date</th>
+      <th scope="col">Documents</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td></td>
+      <td></td>
+      <td><button class="btn btn-primary" type="submit">View</button></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td></td>
+      <td></td>
+      <td><button class="btn btn-primary" type="submit">View</button></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td></td>
+      <td></td>
+      <td><button class="btn btn-primary" type="submit">View</button></td>
+    </tr>
+  </tbody>
+</table>
+</div>
+  </div>;
 }
 export default MyLands;
